f9695d3fe9574110cd343ead844f396e685587c9
nikhil <nikhil.rane@wissen.com>
2025-07-31 23:00:09 +0530

    Init

---
+++ CalculatorMain.java
@@ -0,0 +1,26 @@
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int number1=5;
+        int number2=10;
+        if (args.length > 0) {
+            try {
+                number1 = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //Ignore
+            }
+        }
+        if (args.length > 1) {
+            try {
+                number2 = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //Ignore
+            }
+        }
+
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(number1, number2));
+
+    }
+}
